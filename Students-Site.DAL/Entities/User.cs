afca3b5ea6e7ad40a83ce64832b8d3cc22d366c0
Igor Maxsimov <exanimatesi@yandex.ru>
2019-03-14 11:33:08 +0300

    Add properties for the User entity (for #2)

--- Students-Site.DAL/Entities/User.cs
+++ Students-Site.DAL/Entities/User.cs
@@ -6,5 +6,13 @@ namespace Students_Site.DAL.Entities
 {
     class User
     {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int RoleId { get; set; }
+
+        public Role Role { get; set; }
     }
 }
