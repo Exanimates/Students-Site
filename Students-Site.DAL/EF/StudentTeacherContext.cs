06b95bb829309de0202d8cab57859a875168f941
Igor Maxsimov <exanimatesi@yandex.ru>
2019-03-14 16:43:07 +0300

    Add StudentTeacherContext (for #2)

---
+++ Students-Site.DAL/EF/StudentTeacherContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Students_Site.DAL.Entities;
+
+namespace Students_Site.DAL.EF
+{
+    public class StudentTeacherContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+
+        public StudentTeacherContext(DbContextOptions<StudentTeacherContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<StudentTeacher>()
+                .HasKey(t => new { t.StudentId, t.TeacherId });
+
+            modelBuilder.Entity<StudentTeacher>()
+                .HasOne(sc => sc.Student)
+                .WithMany(s => s.StudentTeachers)
+                .HasForeignKey(sc => sc.StudentId);
+
+            modelBuilder.Entity<StudentTeacher>()
+                .HasOne(sc => sc.Teacher)
+                .WithMany(c => c.StudentTeachers)
+                .HasForeignKey(sc => sc.StudentId);
+        }
+    }
+}
