9ea97ed9a27cf0f3123e69de0e80ab414a2bb053
Igor Maxsimov <exanimatesi@yandex.ru>
2019-03-14 11:41:54 +0300

    Add properties for the Student entity (for #2)

--- Students-Site.DAL/Entities/Student.cs
+++ Students-Site.DAL/Entities/Student.cs
@@ -6,5 +6,9 @@ namespace Students_Site.DAL.Entities
 {
     class Student
     {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+
+        public User User { get; set; }
     }
 }
