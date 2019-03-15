8050ab8bb5fdcbb7dcacbc490df7e6a8af46b2b3
Igor Maxsimov <exanimatesi@yandex.ru>
2019-03-15 17:16:15 +0300

    Add a collection of teachers for the entity subject (for #2)

--- Students-Site.DAL/Entities/Subject.cs
+++ Students-Site.DAL/Entities/Subject.cs
@@ -8,5 +8,7 @@ namespace Students_Site.DAL.Entities
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        public ICollection<Teacher> Teachers { get; set; }
     }
 }
