17acb211c2032b629f575f0fb991cb8ce1c6763b
Igor Maxsimov <exanimatesi@yandex.ru>
2019-03-15 17:49:34 +0300

    Add IUserService (for #3)

---
+++ Students-Site.BLL/Interfaces/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Students_Site.BLL.Business_Logic_Models;
+
+namespace Students_Site.BLL.Interfaces
+{
+    public interface IUserService
+    {
+        void MakeUser(UserBLL orderDto);
+        UserBLL GetUser(int? id);
+        IEnumerable<UserBLL> GetUsers();
+        IEnumerable<StudentBLL> GetStudents();
+        IEnumerable<TeacherBLL> GetTeachers();
+        void Dispose();
+    }
+}
