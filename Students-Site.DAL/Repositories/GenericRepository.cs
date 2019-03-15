d1e8969f5ca50ab463e7cf420311af118eeef6a7
Igor Maxsimov <exanimatesi@yandex.ru>
2019-03-15 13:03:30 +0300

    Add GenericRepository (for #2)

---
+++ Students-Site.DAL/Repositories/GenericRepository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Students_Site.DAL.EF;
+using Students_Site.DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Students_Site.DAL.Repositories
+{
+    public class GenericRepository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        internal ApplicationContext context;
+        internal DbSet<TEntity> dbSet;
+
+        public GenericRepository(ApplicationContext context)
+        {
+            this.context = context;
+            dbSet = context.Set<TEntity>();
+        }
+
+        public virtual IEnumerable<TEntity> GetAll()
+        {
+            return dbSet;
+        }
+
+        public virtual TEntity Get(int id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public virtual void Create(TEntity entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public virtual void Update(TEntity entityToUpdate)
+        {
+            dbSet.Attach(entityToUpdate);
+            context.Entry(entityToUpdate).State = EntityState.Modified;
+        }
+
+        public IEnumerable<TEntity> Find(Func<TEntity, Boolean> predicate)
+        {
+            return dbSet.Where(predicate).ToList();
+        }
+
+        public virtual void Delete(int id)
+        {
+            TEntity entityToDelete = dbSet.Find(id);
+            if (entityToDelete != null)
+                dbSet.Remove(entityToDelete);
+        }
+    }
+}
