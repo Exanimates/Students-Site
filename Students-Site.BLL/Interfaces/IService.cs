2ea43a88d6feade7fe19ed14d0c0b551bf72b3bf
Igor Maxsimov <exanimatesi@yandex.ru>
2019-03-17 11:16:42 +0300

    Update UserService (for #3)

---
+++ Students-Site.BLL/Interfaces/IService.cs
@@ -0,0 +1,13 @@
+﻿using Students_Site.DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Students_Site.BLL.Interfaces
+{
+    public interface IService
+    {
+        IUnitOfWork Database { get; set; }
+        void Dispose();
+    }
+}
--- Students-Site.BLL/Interfaces/IUserService.cs
+++ Students-Site.BLL/Interfaces/IUserService.cs
@@ -1,17 +1,15 @@
-﻿using System;
+﻿    using System;
 using System.Collections.Generic;
 using System.Text;
 using Students_Site.BLL.Business_Logic_Models;
 
 namespace Students_Site.BLL.Interfaces
 {
-    public interface IUserService
+    public interface IUserService : IService
     {
-        void MakeUser(UserBLL orderDto);
+        void MakeUser(UserBLL userBll);
         UserBLL GetUser(int? id);
+        UserBLL UpdateUser(UserBLL userBll);
         IEnumerable<UserBLL> GetUsers();
-        IEnumerable<StudentBLL> GetStudents();
-        IEnumerable<TeacherBLL> GetTeachers();
-        void Dispose();
     }
 }
