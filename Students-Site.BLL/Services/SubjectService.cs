1981395b493f4917e0fd8d976ed4ccbb6da016ae
Igor Maxsimov <exanimatesi@yandex.ru>
2019-03-17 12:16:33 +0300

    Add SubjectService (for #3)

---
+++ Students-Site.BLL/Services/SubjectService.cs
@@ -0,0 +1,61 @@
+﻿using Students_Site.DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Students_Site.BLL.Business_Logic_Models;
+using Students_Site.BLL.Helpers;
+using Students_Site.DAL.Entities;
+using System.Linq;
+using Students_Site.BLL.Interfaces;
+
+namespace Students_Site.BLL.Services
+{
+    public class SubjectService: ISubjectService
+    {
+        IUnitOfWork Database { get; set; }
+
+        public SubjectService(IUnitOfWork unitOfWork)
+        {
+            Database = unitOfWork;
+        }
+
+        public void MakeSubject(SubjectBLL subjectBll)
+        {
+            Subject subjectByName = Database.SubjectRepository.GetAll().FirstOrDefault(r => r.Name == subjectBll.Name);
+
+            if (subjectByName != null)
+                throw new ValidationException("Такой предмет уже существует", "");
+
+            Subject subject = new Subject
+            {
+                Name = subjectBll.Name
+            };
+            Database.SubjectRepository.Create(subject);
+            Database.Save();
+        }
+
+        public SubjectBLL GetSubject(int? id)
+        {
+            if (id == null)
+                throw new ValidationException("Id предмета не установлено", "");
+            var role = Database.RoleRepository.Get(id.Value);
+            if (role == null)
+                throw new ValidationException("Предмет не найден", "");
+
+            return new SubjectBLL { Name = role.Name };
+        }
+
+        public IEnumerable<SubjectBLL> GetSubjects()
+        {
+            return Database.SubjectRepository.GetAll().Select(subject => new SubjectBLL
+            {
+                Name = subject.Name
+            });
+        }
+
+        public void Dispose()
+        {
+            Database.Dispose();
+        }
+    }
+}
