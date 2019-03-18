02c25cf12772c6c20e321233992b1b64927ba6c3
Igor Maxsimov <exanimatesi@yandex.ru>
2019-03-18 07:33:06 +0300

    Add register options for ApplicationContext in ServiceModule (for #3)

--- Students-Site.BLL/Infrastructure/ServiceModule.cs
+++ Students-Site.BLL/Infrastructure/ServiceModule.cs
@@ -1,5 +1,6 @@
 ﻿using Autofac;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Students_Site.DAL.EF;
 using Students_Site.DAL.Interfaces;
 using Students_Site.DAL.UnitOfWork;
@@ -10,11 +11,20 @@ namespace Students_Site.BLL.Infrastructure
     {
         protected override void Load(ContainerBuilder builder)
         {
+            builder.Register(c =>
+            {
+                var config = c.Resolve<IConfiguration>();
+                var options = new DbContextOptionsBuilder<ApplicationContext>();
+                options.UseSqlServer(config.GetSection("ConnectionString:DefaultConnection").Value);
+
+                return new ApplicationContext(options.Options);
+            }).AsSelf().InstancePerLifetimeScope();
+
             builder.RegisterType<UnitOfWork>()
                .As<IUnitOfWork>()
                .InstancePerLifetimeScope();
 
-            builder.RegisterType<ApplicationContext>().AsSelf();
+            base.Load(builder);
         }
     }
 }
