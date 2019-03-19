48cdfd784e225b9981d2226948255bed377f8ee4
Igor Maxsimov <exanimatesi@yandex.ru>
2019-03-19 09:31:56 +0300

    Add Login for init users (for #2)

--- Students-Site.DAL/EF/ApplicationContext.cs
+++ Students-Site.DAL/EF/ApplicationContext.cs
@@ -33,9 +33,9 @@ namespace Students_Site.DAL.EF
 
             var users = new List<User>
             {
-                new User { Id = 1, FirstName = "Petr", Password = "123", LastName = "Ivanov", RoleId = roles[0].Id },
-                new User { Id = 2, FirstName = "Andrey", Password = "1488", LastName = "Petrov", RoleId = roles[1].Id },
-                new User { Id = 3, FirstName = "Oleg", Password = "8814", LastName = "Kotlov", RoleId = roles[2].Id },
+                new User { Id = 1, Login = "King", FirstName = "Petr", Password = "123", LastName = "Ivanov", RoleId = roles[0].Id },
+                new User { Id = 2, Login = "Artas", FirstName = "Andrey", Password = "1488", LastName = "Petrov", RoleId = roles[1].Id },
+                new User { Id = 3, Login = "Shaman" ,FirstName = "Oleg", Password = "8814", LastName = "Kotlov", RoleId = roles[2].Id },
             };
 
             builder.Entity<Role>().HasData(roles);
