8d3d66769f55a154cddde2b92706c20fc27f6bee
Igor Maxsimov <exanimatesi@yandex.ru>
2019-03-19 11:54:39 +0300

    Add TeachersController (for #4)

---
+++ Students-Site/Controllers/TeachersController.cs
@@ -0,0 +1,54 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Students_Site.BLL.Services;
+using Students_Site.Models;
+
+namespace Students_Site.Controllers
+{
+    public class TeachersController : Controller
+    {
+        ITeacherService _teacherService;
+        IUserService _userService;
+        ISubjectService _subjectService;
+
+        public TeachersController(ITeacherService teacherService, IUserService userService, ISubjectService subjectService)
+        {
+            _teacherService = teacherService;
+            _userService = userService;
+            _subjectService = subjectService;
+        }
+
+        public IActionResult Index()
+        {
+            var teacherBll = _teacherService.GetTeachers();
+
+            var users = _userService.GetUsers();
+
+            var teachers = teacherBll
+                .Join(
+                    users,
+                    t => t.UserId,
+                    u => u.Id,
+                    (t, u) => new TeacherModel
+                    {
+                        Id = t.Id,
+                        FirstName = u.FirstName,
+                        LastName = u.LastName,
+                        Login = u.Login,
+                        Password = u.Password,
+                        RoleId = u.RoleId,
+                        SubjectId = t.SubjectId,
+                        SubjectName = _subjectService.GetSubject(t.SubjectId).Name
+                    });
+
+            return View(teachers);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _teacherService.Dispose();
+            _userService.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Students-Site/Models/TeacherModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Students_Site.Models
+{
+    public class TeacherModel : UserModel
+    {
+        public int SubjectId;
+        public string SubjectName;
+    }
+}
