dd4f13d9c03809bca65130b5f72780f63c688a42
Igor Maxsimov <exanimatesi@yandex.ru>
2019-03-19 11:54:21 +0300

    Add StudentsController (for #4)

---
+++ Students-Site/Controllers/StudentsController.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Students_Site.BLL.Services;
+using Students_Site.Models;
+
+namespace Students_Site.Controllers
+{
+    public class StudentsController : Controller
+    {
+        IStudentService _studentService;
+        IUserService _userService;
+
+        public StudentsController(IStudentService studentService, IUserService userService)
+        {
+            _studentService = studentService;
+            _userService = userService;
+        }
+
+        public IActionResult Index()
+        {
+            var studentsBll = _studentService.GetStudents();
+
+            var users = _userService.GetUsers();
+
+            var students = studentsBll
+                .Join(
+                    users,
+                    s => s.UserId,
+                    u => u.Id,
+                    (s, u) => new StudentModel
+                    {
+                        Id = s.Id,
+                        FirstName = u.FirstName,
+                        LastName = u.LastName,
+                        Login = u.Login,
+                        Password = u.Password,
+                        RoleId = u.RoleId
+                    });
+
+            return View(students);
+        }
+    }
+}
---
+++ Students-Site/Models/StudentModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Students_Site.Models
+{
+    public class StudentModel : UserModel
+    {
+        
+    }
+}
