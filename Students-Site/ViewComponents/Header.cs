c76fc012c22496a6a6f9c53433756f22a8335c8c
Igor Maxsimov <maxsimovigorfullstack@yandex.ru>
2019-03-25 10:08:21 +0300

    Added header view component (for #4)

---
+++ Students-Site/ViewComponents/Header.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Students_Site.BLL.Services;
+using Students_Site.Models.Home;
+
+namespace Students_Site.ViewComponents
+{
+    public class Header : ViewComponent
+    {
+        private readonly ITeacherService _teacherService;
+        private readonly IStudentService _studentService;
+
+        public Header(IStudentService studentService, ITeacherService teacherService)
+        {
+            _teacherService = teacherService;
+            _studentService = studentService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var indexModel = new IndexModel
+            {
+                StudentCount = _studentService.GetStudents().Count(),
+                TeacherCount = _teacherService.GetTeachers().Count()
+            };
+
+            return View("Header", indexModel);
+        }
+    }
+}
---
+++ Students-Site/wwwroot/js/current-time.js
@@ -0,0 +1,18 @@
+﻿function startTime(elemId) {
+	var spanCurrent = document.getElementById(elemId);
+
+	function getTime() {
+		var today = new Date();
+		var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+		var time = today.getHours() + ":" + checkTime(today.getMinutes()) + ":" + checkTime(today.getSeconds());
+
+		spanCurrent.textContent = date + ' ' + time;
+	}
+
+	setInterval(getTime, 500);
+}
+
+function checkTime(i) {
+	if (i < 10) {i = "0" + i};
+	return i;
+}
