aabfeef71b7829460001248294ad5407766058b0
Igor Maxsimov <maxsimovigorfullstack@yandex.ru>
2019-03-25 17:33:35 +0300

    Made it so that sorting was only for those columns that have a header (for #4)

--- Students-Site/wwwroot/js/startDataTable.js
+++ Students-Site/wwwroot/js/startDataTable.js
@@ -1,9 +1,12 @@
 ﻿$(document).ready(function () {
+	var emptyCellCount = 0;
 	$('#example thead tr').clone(true).appendTo('#example thead');
 	$('#example thead tr:eq(1) th').each(function (i) {
 		var title = $(this).text();
 		if (title !== "") {
 			$(this).html('<input type="text" placeholder="Поиск ' + title + '" />');
+		} else {
+			emptyCellCount++;
 		}
 
 		$('input', this).on('keyup change', function () {
@@ -16,15 +19,17 @@
 		});
 	});
 
+	var columnDefs = [];
+	for (var i = 1; i <= emptyCellCount; i++) {
+		columnDefs.push({ "orderable": false, "targets": $('#example tfoot th').length - i });
+	}
+
 	// DataTable
 	var table = $('#example').DataTable({
 		"language": {
 			"url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Russian.json"
 		},
-		"columnDefs": [
-			{ "orderable": false, "targets": $('#example tfoot th').length - 1 },
-			{ "orderable": false, "targets": $('#example tfoot th').length - 2 }
-		],
+		"columnDefs": columnDefs,
 		orderCellsTop: true,
 		fixedHeader: true
 	});
