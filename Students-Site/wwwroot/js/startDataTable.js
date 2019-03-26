24b16b80d9bbee79df5408c6ee45ee6aae07026a
Igor Maxsimov <maxsimovigorfullstack@yandex.ru>
2019-03-26 09:03:45 +0300

    Added jquery datatable for edit/create pages (for #4)

--- Students-Site/wwwroot/js/startDataTable.js
+++ Students-Site/wwwroot/js/startDataTable.js
@@ -1,12 +1,12 @@
 ﻿$(document).ready(function () {
-	var emptyCellCount = 0;
+	var emptyCells = [];
 	$('#example thead tr').clone(true).appendTo('#example thead');
 	$('#example thead tr:eq(1) th').each(function (i) {
 		var title = $(this).text();
 		if (title !== "") {
 			$(this).html('<input type="text" placeholder="Поиск ' + title + '" />');
 		} else {
-			emptyCellCount++;
+			emptyCells.push(i);
 		}
 
 		$('input', this).on('keyup change', function () {
@@ -20,12 +20,13 @@
 	});
 
 	var columnDefs = [];
-	for (var i = 1; i <= emptyCellCount; i++) {
-		columnDefs.push({ "orderable": false, "targets": $('#example tfoot th').length - i });
+	for (var i = 0; i < emptyCells.length; i++) {
+		columnDefs.push({ "orderable": false, "targets": emptyCells[i]});
 	}
 
 	// DataTable
 	var table = $('#example').DataTable({
+		order: [],
 		"language": {
 			"url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Russian.json"
 		},
