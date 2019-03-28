b2fa51c563eac6b0d86387fb34b1a1e00db654ed
Igor Maxsimov <maxsimovigorfullstack@yandex.ru>
2019-03-28 15:10:47 +0300

    Added extension methods to find out the role of the current user (for #5)

---
+++ Extensions/Principal/PrincipalExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Claims;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Extensions.Principal
+{
+    public static class PrincipalExtensions
+    {
+        public static bool IsDean(this ClaimsPrincipal claimsPrincipal)
+        {
+            return claimsPrincipal.IsInRole("1");
+        }
+
+        public static bool IsStudent(this ClaimsPrincipal claimsPrincipal)
+        {
+            return claimsPrincipal.IsInRole("2");
+        }
+
+        public static bool IsTeacher(this ClaimsPrincipal claimsPrincipal)
+        {
+            return claimsPrincipal.IsInRole("3");
+        }
+    }
+}
