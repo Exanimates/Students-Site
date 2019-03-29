7e96c9d336ea5735b30d667ed2f5a5a363f28e5d
Igor Maxsimov <maxsimovigorfullstack@yandex.ru>
2019-03-29 10:27:50 +0300

    Updated startDataTable script (for #5)

--- Students-Site/wwwroot/js/startDataTable.js
+++ Students-Site/wwwroot/js/startDataTable.js
@@ -1,37 +1,42 @@
 ﻿$(document).ready(function () {
-	var emptyCells = [];
-	$('#example thead tr').clone(true).appendTo('#example thead');
-	$('#example thead tr:eq(1) th').each(function (i) {
-		var title = $(this).text();
-		if (title !== "") {
-			$(this).html('<input type="text" placeholder="Поиск" />');
-		} else {
-			emptyCells.push(i);
-		}
-
-		$('input', this).on('keyup change', function () {
-			if (table.column(i).search() !== this.value) {
-				table
-					.column(i)
-					.search(this.value)
-					.draw();
-			}
-		});
-	});
-
-	var columnDefs = [];
-	for (var i = 0; i < emptyCells.length; i++) {
-		columnDefs.push({ "orderable": false, "targets": emptyCells[i]});
-	}
-
-	// DataTable
-	var table = $('#example').DataTable({
-		order: [],
-		"language": {
-			"url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Russian.json"
-		},
-		"columnDefs": columnDefs,
-		orderCellsTop: true,
-		fixedHeader: true
-	});
+    startDataTable($("table[name='dataTable']"));
 });
+
+function startDataTable(table) {
+    var emptyCells = [];
+
+    $(table).find("thead tr").clone(true).appendTo($(table).find("thead"));
+    $(table).find("thead tr:eq(1) th").each(function (i) {
+        var title = $(this).text();
+        if (title !== "") {
+            $(this).html('<input type="text" placeholder="Поиск" />');
+        } else {
+            emptyCells.push(i);
+        }
+
+        $('input', this).on('keyup change', function () {
+            if (table.column(i).search() !== this.value) {
+                table
+                    .column(i)
+                    .search(this.value)
+                    .draw();
+            }
+        });
+    });
+
+    var columnDef = [];
+    for (var i = 0; i < emptyCells.length; i++) {
+        columnDef.push({ "orderable": false, "targets": emptyCells[i] });
+    }
+
+
+    $(table).DataTable({
+        order: [],
+        "language": {
+            "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Russian.json"
+        },
+        "columnDefs": columnDef,
+        orderCellsTop: true,
+        fixedHeader: true
+    });
+}
